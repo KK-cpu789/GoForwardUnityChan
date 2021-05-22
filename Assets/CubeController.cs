1492b2ad1c6cebdf5edc81b3874ba10a74b32613
KK-cpu789 <82213731+KK-cpu789@users.noreply.github.com>
2021-05-22 23:50:44 +0900

    Recreated the Project

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -29,4 +29,9 @@ public class CubeController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GetComponent<AudioSource>().Play();
+    }
 }
